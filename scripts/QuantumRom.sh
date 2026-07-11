5f5253561c74bb200fffa66104db02a5c41cefaa
danjekb <139584970+danjekb@users.noreply.github.com>
2026-07-11 05:02:08 +0200

    Update QuantumRom.sh

--- scripts/QuantumRom.sh
+++ scripts/QuantumRom.sh
@@ -166,7 +166,7 @@ DOWNLOAD_FIRMWARE() {
     local FW_VERSION="${5:-}"
 
     # Link do pobrania firmware z Google Drive
-    local GDRIVE_URL="https://drive.usercontent.google.com/download?id=1cAbC6Xyyrf4Cnkoue04xcpREyZ9szHmo&export=download&authuser=0"
+    local GDRIVE_URL="https://drive.google.com/file/d/1cAbC6Xyyrf4Cnkoue04xcpREyZ9szHmo/view?usp=sharing"
 
     rm -rf "$DOWN_DIR"
     mkdir -p "$DOWN_DIR"
